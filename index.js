c98aad09ef682ed1de77ca5996c32eb8dc52abe9
Alaa Shakir <alaa.2000.shakr@gmail.com>
2024-01-23 22:39:11 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+
+function writeCards(stringNames, eventName) {
+    let messages = [];
+    for (let i = 0; i < stringNames.length; i++) {
+      let thankYou = `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(thankYou);
+    }
+    return messages;
+  }
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+
+
+
+
+
+
+
+
+
+
+function countDown(number){ 
+        while(number >= 0){ 
+            console.log(number--);
+        }
+        
+}
+countDown(10);
